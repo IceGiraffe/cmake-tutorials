ec57a731e9e12229b51d070aa538768f655878c0
wang <wangfeiyu@pku.edu.cn>
2023-03-28 00:53:42 +0800

    init

---
+++ lib-hello/lib/hello.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+
+#include <iostream>
+
+void hello_from_lib(){
+    std::cout << "Hello from lib." << std::endl;
+}
---
+++ lib-usage/src/main.cpp
@@ -0,0 +1,8 @@
+#include <hello.h>
+
+using namespace std;
+
+int main(){
+    hello_from_lib();
+    return 0;
+}
